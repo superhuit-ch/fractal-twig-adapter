f8b1e70144c3257fd9120e7a89ce3b356704039e
Hugo Messer <hugo@hugomesser.ch>
2018-08-29 01:54:25 +0100

    fix path resolution
    use of `path.join` to avoid unexpected output `///file.txt` (translated http://file.txt by chrome while we want http://localhost/file.txt) when the mountPath is `/`

--- src/filters/path.js
+++ src/filters/path.js
@@ -1,22 +1,23 @@
 'use strict';
 
 const _ = require('lodash');
+const path = require('path');
 const utils = require('@frctl/fractal').utils;
 
 module.exports = function(fractal) {
-	return function(path) {
+	return function(uri) {
 		const env = this.context._env;
 		const request = env && env.request;
 		const mountPath = fractal.web.get('static.mount');
-		let _path;
+		let _uri;
 
 		if (!env || env.server) {
-			_path = path;
-			_path = (mountPath) ? `/${mountPath}${_path}` : _path;
+			_uri = uri;
+			_uri = (mountPath) ? path.join('/', mountPath, _uri) : _uri;
 		} else {
-			_path = utils.relUrlPath(path, _.get(request, 'path', '/'), fractal.web.get('builder.urls'));
+			_uri = utils.relUrlPath(uri, _.get(request, 'path', '/'), fractal.web.get('builder.urls'));
 		}
 
-		return _path;
+		return _uri;
 	};
 };
